f19d62c3de7d09ca8c013fe354318ec98fc2d7ae
Magdiel <magdiel.altynbekov@gmail.com>
2022-10-27 15:55:31 +0600

    qwe

--- HomeWork N1 (2month)/main.swift
+++ HomeWork N1 (2month)/main.swift
@@ -9,23 +9,9 @@ import Foundation
 
 print("Hello, World!")
 
-//var carArray: [Car] = []
-//
-//class Car {
-//    var model = ""
-//    var speed = 0
-//    var power = 0
-//    var hp = 0
-//
-//    func addSet(motor: Int){
-//        for i in 0...28{
-//            carArray.append(Car(speed:"\(i)0"))
-//        }
-//        for i in carArray{
-//            i.addSet(motor: 0)
-//        }
-//    }
-//}
+
+
+
 
 
 class Car {
@@ -59,4 +45,127 @@ for i in 1...28{
 for i in carArray{
     i.printSet()
 }
+
+
+
+
+
+
+class Shop{
+
+    var name = "Мармеладка"
+    var ploshad = 120
+    var personal = 10
+    var products = ["Coca-Cola": 50, "Pepsi": 50, "Хлеб": 25, "Яйца": 100, "Ролтон": 25, "Мука": 100, "Чай": 100]
+    func showProducts(){
+        print(products)
+    }
+
+    func addToShop(name: String, price: Int) {
+        products["\(name)"] = price
+    }
+}
+
+class Product{
+
+    var name: String
+    var price: Int
+
+    init(name: String, price: Int){
+        self.name = name
+        self.price = price
+    }
+}
+
+
+var shop = Shop()
+shop.showProducts()
+
+var sugar = Product(name: "Сахар", price: 70)
+
+shop.addToShop(name: sugar.name, price: sugar.price)
+shop.showProducts()
+
+
+
+
+
+
+class Client {
+    
+    var name: String
+    var surname: String
+    var id: Int
+    var balance: Int
+    
+    init(name: String, surname: String, id: Int, balance: Int) {
+        self.name = name
+        self.surname = surname
+        self.id = id
+        self.balance = balance
+    }
+}
+
+
+class Bank {
+    
+    var name = "KKB"
+    var clients: [Client]
+    
+    init (clients: [Client]){
+    self.clients = clients
+    }
+    
+    // Просмотр баланса по ID
+    func balanceID(id: Int) {
+        for i in clients{
+            if i.id == id   {
+                print("Баланс равен \(i.balance)")
+            }
+        }
+    }
+    
+    // Просмотр баланса по фамилии
+    func balanceSurname (surname: String){
+        for i in clients{
+            if i.surname == surname{
+                print("Баланс равен \(i.balance)")
+            }
+        }
+    }
+    
+    // Перевод денег по имени
+    func perevod(fromName: String, toName: String, summa: Int) {
+        for i in clients{
+            if i.name == fromName{
+                i.balance -= summa
+            }
+            if i.name == toName{
+                i.balance += summa
+            }
+        }
+    }
+    
+}
+
+var client1 = Client(name: "Nuridin", surname: "Safaraliev", id: 111, balance: 1500)
+    
+var client2 = Client(name: "Sadyr", surname: "Japarov", id: 0, balance: 1000000)
+
+var client3 = Client(name: "Petr", surname: "Petrovich", id: 15, balance: 30000)
+
+var bank = Bank(clients: [client1, client2, client3])
+
+
+
+
+bank.balanceID(id: 111)
+
+bank.balanceSurname(surname: "Altynbekov")
+
+bank.perevod(fromName: "Altynbekov", toName: "Bekjan", summa: 1000000)
+
+bank.balanceID(id: 111)
+
+
 
